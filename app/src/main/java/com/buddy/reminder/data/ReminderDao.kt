5e67424525ea564d47c4019ba5da6f6d3741919a
yogesh63623-commits <yogesh63623@gmail.com>
2026-09-09 21:10:09 +0530

    Add methods to retrieve all events and events by range

--- app/src/main/java/com/buddy/reminder/data/ReminderDao.kt
+++ app/src/main/java/com/buddy/reminder/data/ReminderDao.kt
@@ -12,4 +12,10 @@ interface ReminderDao {
 
     @Query("UPDATE internal_events SET isHandled = 1 WHERE id = :id")
     suspend fun markAsHandled(id: Long)
+
+    @Query("SELECT * FROM internal_events ORDER BY eventTimestamp ASC")
+    suspend fun getAllEvents(): List<ReminderEvent>
+
+    @Query("SELECT * FROM internal_events WHERE eventTimestamp >= :start AND eventTimestamp < :end ORDER BY eventTimestamp ASC")
+    suspend fun getEventsForRange(start: Long, end: Long): List<ReminderEvent>
 }
